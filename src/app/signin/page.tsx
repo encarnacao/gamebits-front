b5fcdd5d0d93428f17c588fded0fb606b768e7a1
encarnacao <cencarnacaoq@gmail.com>
2023-06-07 00:29:19 -0300

    feat: add link to signup and homepage

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -3,21 +3,19 @@ import logo from '../../assets/Logo.png';
 import zelda from '../../assets/zelda.jpg';
 import Link from 'next/link';
 import TextInput from '@/components/text-input';
+import GradientImage from '@/components/gradient-image';
 
 export default function SignIn(){
     return (
-    <main className='flex max-h-screen'>
-        <div className='flex flex-col w-3/4 overflow-hidden'>
-            <Image src={logo} className='w-2/5 self-center' alt='Logo' />
+    <main className='flex min-h-screen'>
+        <div className='flex flex-col w-3/4 overflow-hidden max-h-screen'>
+            <Link href='/' className='flex justify-center align-middle'>
+            <Image priority={false} src={logo} className='w-2/5' alt='Logo' />
+            </Link>
             <h1 className='self-center text-2xl'>Jogue, registre e compartilhe</h1>
-            <div className='relative'>
-                <Image src={zelda} alt='Zelda'/>
-                <div className='absolute inset-0 bg-gradient-radial from-transparent to-black'></div>
-                <div className='absolute inset-0 bg-gradient-to-t from-transparent via-transparent to-black'></div>
-                <div className='absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black'></div>
-            </div>
+            <GradientImage src={zelda} alt='Zelda' />
         </div>
-        <div className='flex flex-col w-1/4 p-10 bg-slate-900 justify-center'>
+        <div className='flex flex-col w-1/4 p-10 bg-slate-900 justify-evenly'>
             <h1 className='self-center text-2xl'>Faça seu login</h1>
             <form className='flex flex-col gap-8'>
             <div className='flex flex-col'>
@@ -35,7 +33,7 @@ export default function SignIn(){
                 <button className='bg-orange-700 text-white p-2 rounded-lg mt-5'>Steam</button>
                 <button className='bg-orange-700 text-white p-2 rounded-lg mt-5'>Google</button>
             </div>
-            <h2 className='self-center text-white mt-10'>Não tem uma conta? <Link href='#' className='text-blue-500'>Cadastre-se</Link></h2>
+            <h2 className='self-center text-white mt-10'>Não tem uma conta? <Link href='/signup' className='text-blue-500'>Cadastre-se</Link></h2>
         </div>
     </main>
     )
