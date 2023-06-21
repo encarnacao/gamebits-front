235ceb9edb90eec78d935e8c8afb3c8256fcd930
encarnacao <cencarnacaoq@gmail.com>
2023-06-21 15:38:51 -0300

    feat: add search to header

--- src/components/game-card.tsx
+++ src/components/game-card.tsx
@@ -2,21 +2,30 @@ import { GameData } from "@/pages/search";
 import Image from "next/image";
 
 export default function GameCard({ data }: { data: GameData }) {
-  const summaryClassNames = `text-sm text-white overflow-hidden`;
   return (
-    <div className="flex w-full overflow-hidden rounded-lg h-52 bg-slate-900 my-4">
-      <Image src={data.coverUrl} className="object-cover" alt="Game Cover" width={132} height={208} />
+    <div className="flex w-full overflow-hidden rounded-lg h-52 bg-slate-900 my-4 transition-all hover:bg-slate-800">
+      <Image
+        src={data.coverUrl}
+        className="object-cover"
+        alt="Game Cover"
+        width={132}
+        height={208}
+      />
       <div className="flex flex-col w-full p-4">
         <div className="flex justify-between">
-          <h1 className="text-2xl font-bold text-white">{data.name}</h1>
+          <h1 className="text-2xl font-bold">{data.name}</h1>
           <div>
-            <h2 className="text-l font-bold text-white">
-              {data.platformNames}
-            </h2>
-            <h3 className="text-l font-bold text-white">{data.releaseDate}</h3>
+            <h2 className="text-l font-bold">{data.platformNames}</h2>
+            <h3 className="text-l font-bold">
+              {data.releaseDate === "Invalid Date"
+                ? "Não Lançado"
+                : data.releaseDate}
+            </h3>
           </div>
         </div>
-        <p className={summaryClassNames}>{data.summary}</p>
+        <p className="text-sm text-ellipsis overflow-hidden text-left">
+          {data.summary}
+        </p>
       </div>
     </div>
   );
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,8 +5,12 @@ import Image from "next/image";
 import logo from "../assets/Logo.png";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
+import { useRouter } from "next/router";
 
 export default function Header({ className }: { className?: string }) {
+  const [search, setSearch] = useState("");
+  const router = useRouter();
   const navlink =
     "text-slate-100 font-bold hover:text-orange-500 transition-all";
   const pathname = usePathname();
@@ -28,6 +32,14 @@ export default function Header({ className }: { className?: string }) {
       path: "#",
     },
   ];
+
+  const handleSubmit = () => {
+    router.push({
+      pathname: "/search",
+      query: { name: search },
+    });
+  };
+
   if (pathname === "/signin" || pathname == "/signup") return null;
   return (
     <header
@@ -45,10 +57,20 @@ export default function Header({ className }: { className?: string }) {
         <input
           className="bg-slate-800/70 p-4 h-7 pr-12 pl-4 outline-none rounded-full placeholder-slate-600"
           type="text"
-          placeholder="Search"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              handleSubmit();
+            }
+          }}
+          placeholder="Buscar"
         />
-        <div className="absolute inset-y-0 right-5 flex items-center pl-4">
-          <MagnifyingGlassIcon className="h-5 w-5 text-gray-400" />
+        <div className="absolute inset-y-0 right-5 flex items-center pl-4 cursor-pointer">
+          <MagnifyingGlassIcon
+            onClick={handleSubmit}
+            className="h-5 w-5 text-gray-400"
+          />
         </div>
       </div>
     </header>
