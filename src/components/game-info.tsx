48fb15da7716a87bd2050b2ae3ed5d2f44ddc722
encarnacao <cencarnacaoq@gmail.com>
2023-06-22 23:39:07 -0300

    refactor: move game information to component

---
+++ src/components/game-info.tsx
@@ -0,0 +1,35 @@
+import { SingleGame } from "@/types";
+import dayjs from "dayjs";
+
+export default function GameInfo({ gameData }: { gameData: SingleGame }) {
+  return (
+    <div className="flex w-3/5 items-center justify-between flex-1 p-10 text-center mt-52 bg-slate-950 mx-auto border-b-4 border-slate-600">
+      <div className="flex items-center">
+        <img
+          src={gameData.cover_url}
+          className="w-32 absolute top-40 border-t-4 border-l-4 border-orange-500"
+          alt="capa"
+        />
+        <div className="ml-40 text-left">
+          <h1 className="text-4xl text-slate-200 serifed ml-8">
+            {gameData.name}
+          </h1>
+          <h1 className="text-xl text-slate-200 serifed ml-8">
+            Plataformas: {gameData.platforms}
+          </h1>
+        </div>
+      </div>
+      <div>
+        <div className="text-right">
+          <h2 className="text-xl text-slate-200 serifed mb-4">
+            Lançamento:{" "}
+            {dayjs(gameData.original_release_date).format("DD/MM/YYYY")}
+          </h2>
+          <h2 className="text-xl text-slate-200 serifed mb-4">
+            Gêneros: {gameData.genres}
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/game/[id].tsx
+++ src/pages/game/[id].tsx
@@ -1,60 +1,44 @@
-import { useRouter } from "next/router";
-import { SingleGame } from "@/types";
+import { GameBooleans, SingleGame } from "@/types";
 import { GetServerSideProps, InferGetServerSidePropsType } from "next";
-import axios from "axios";
-import dayjs from "dayjs";
 import { getGameById } from "@/api";
+import GameInfo from "@/components/game-info";
+import { parseCookies } from "nookies";
+import { CheckGameLibrary } from "@/helpers";
 
 export default function GamePage({
   gameData,
+  booleans,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  const router = useRouter();
   if (!gameData) {
-    return <div>Game not found</div>;
+    return (
+      <div className="flex w-3/5 items-center justify-between p-10 text-center mt-40 bg-slate-950 mx-auto border-b-4 border-slate-600">
+        Game not found
+      </div>
+    );
   }
   return (
-    <main>
-      <div className="flex w-3/5 items-center justify-between flex-1 p-10 text-center mt-52 bg-slate-950 mx-auto border-b-4 border-slate-600">
-        <div className="flex items-center">
-          <img
-            src={gameData.cover_url}
-            className="w-32 absolute top-40 border-t-4 border-l-4 border-orange-500"
-            alt="capa"
-          />
-          <div className="ml-40 text-left">
-            <h1 className="text-4xl text-slate-200 serifed ml-8">
-              {gameData.name}
-            </h1>
-            <h1 className="text-xl text-slate-200 serifed ml-8">
-              Plataformas: {gameData.platforms}
-            </h1>
-          </div>
-        </div>
-        <div>
-          <div className="text-right">
-            <h2 className="text-xl text-slate-200 serifed mb-4">
-              Lançamento:{" "}
-              {dayjs(gameData.original_release_date).format("DD/MM/YYYY")}
-            </h2>
-            <h2 className="text-xl text-slate-200 serifed mb-4">
-              Gêneros: {gameData.genres}
-            </h2>
-          </div>
-        </div>
-      </div>
+    <main className="flex flex-col justify-center items-center">
+      <GameInfo gameData={gameData} />
+      {JSON.stringify(booleans)}
     </main>
   );
 }
 
 export const getServerSideProps: GetServerSideProps<{
   gameData: SingleGame | null;
+  booleans: GameBooleans | null;
 }> = async (context) => {
   const params = context.params;
+  const { id: userId } = parseCookies(context);
   const id = params?.id as string;
   const gameData = await getGameById(id);
+  const booleans = gameData
+    ? await CheckGameLibrary(Number(userId), gameData.id)
+    : null;
   return {
     props: {
       gameData,
+      booleans,
     },
   };
 };
