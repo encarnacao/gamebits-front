1828dc72a766f0d211cc32d6a89e3982e02b90ec
encarnacao <cencarnacaoq@gmail.com>
2023-06-22 22:17:11 -0300

    feat: add routing

--- src/components/game-card.tsx
+++ src/components/game-card.tsx
@@ -1,25 +1,41 @@
-import { GameData } from "@/pages/search";
+import { GameData } from "@/types";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function GameCard({ data }: { data: GameData }) {
+  const router = useRouter();
+  const releasedGame = data.releaseDate !== "Invalid Date";
+  const handleClick = () => {
+    if (!releasedGame) return;
+    router.push(`/game/${data.id}`);
+  };
   return (
     <div className="flex w-full overflow-hidden rounded-lg h-52 bg-slate-900 my-4 transition-all hover:bg-slate-800">
       <Image
         src={data.coverUrl}
-        className="object-cover"
+        className={"object-cover" + (releasedGame ? " cursor-pointer" : "")}
+        onClick={handleClick}
         alt="Game Cover"
         width={132}
         height={208}
       />
       <div className="flex flex-col w-full p-4">
         <div className="flex justify-between">
-          <h1 className="text-2xl font-bold">{data.name}</h1>
+          <h1
+            className={
+              "text-2xl font-bold transition-all " +
+              (releasedGame
+                ? "hover:text-orange-500 cursor-pointer"
+                : "hover:text-red-600")
+            }
+            onClick={handleClick}
+          >
+            {data.name}
+          </h1>
           <div>
             <h2 className="text-l font-bold">{data.platformNames}</h2>
             <h3 className="text-l font-bold">
-              {data.releaseDate === "Invalid Date"
-                ? "Não Lançado"
-                : data.releaseDate}
+              {releasedGame ? data.releaseDate : "Lançamento não definido"}
             </h3>
           </div>
         </div>
