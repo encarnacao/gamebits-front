75656231dbcf381749b179393b16920751ff48ae
encarnacao <cencarnacaoq@gmail.com>
2023-06-25 16:00:26 -0300

    feat: return empty array instead of null if error

--- src/api/library-requests.ts
+++ src/api/library-requests.ts
@@ -5,23 +5,23 @@ axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_URL;
 
 export async function getUserLibrary(
   userId: number
-): Promise<LibraryEntry[] | null> {
+): Promise<LibraryEntry[]> {
   try {
     const response = await axios.get(`/libraries/${userId}`);
     return response.data;
   } catch {
-    return null;
+    return [];
   }
 }
 
 export async function getUserWishlist(
   userId: number
-): Promise<LibraryEntry[] | null> {
+): Promise<LibraryEntry[]> {
   try {
     const response = await axios.get(`/libraries/wishlist/${userId}`);
     return response.data;
   } catch {
-    return null;
+    return [];
   }
 }
 
