0782a2f4a5e5283f230aec5f7f5552bc67843aa2
encarnacao <cencarnacaoq@gmail.com>
2023-06-26 16:10:30 -0300

    feat: add review requests

--- src/api/review-requests.ts
+++ src/api/review-requests.ts
@@ -1,3 +1,4 @@
+import { GameReviews, UserReviews } from "@/types";
 import axios from "axios";
 
 axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_URL;
@@ -21,5 +22,38 @@ export async function postReview(
     console.log(err);
     alert("Error posting review");
     return false;
+  }
+}
+
+export async function deleteReview(token: string, reviewId: number) {
+  try {
+    await axios.delete(`/reviews/${reviewId}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return true;
+  } catch (err) {
+    console.log(err);
+    alert("Error deleting review");
+    return false;
+  }
+}
+
+export async function getGameReviews(gameId: number): Promise<GameReviews[]> {
+  try {
+    const request = await axios.get(`/reviews/${gameId}`);
+    return request.data;
+  } catch {
+    return [];
+  }
+}
+
+export async function getUserReviews(userId: number): Promise<UserReviews[]> {
+  try {
+    const request = await axios.get(`/reviews/user/${userId}`);
+    return request.data;
+  } catch {
+    return [];
   }
 }
